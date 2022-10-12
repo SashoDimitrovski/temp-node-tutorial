74b1fd73ecf8e9cf862cba62cd64f1e9efd12efe
Saso Dimitrovski <sasodimitrovski1982@yahoo.com>
2022-10-12 23:49:27 +0200

    first commit

---
+++ os-modules.js
@@ -0,0 +1,9 @@
+const names = require('./names');
+const sayHi = require('./utils')
+const data = require('./alternative');
+
+require('./mind-granate');
+
+sayHi('Saso');
+sayHi(names.john);
+sayHi(names.peter);
